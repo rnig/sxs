e6480ecd699b5d1a0a8daeae609414687aa0572a
Erwin Geeraerts, bestuurder rnig bv <8044437+rnig@users.noreply.github.com>
2023-07-26 05:01:43 +0200

    Module head is a first attempt at imitating unix head.

--- src/head.py
+++ src/head.py
@@ -1,5 +1,25 @@
 import sys
 
+#
+# De module head is een imitatie van de unix head.
+#
+#
+# Bijv:
+#
+# sxs) d:\sxs>py src\head.py 5 dat\out\sample-cy.xml
+# <?xml
+#
+# (sxs) d:\sxs>py src\head.py 15 dat\out\sample-cy.xml
+# <?xml version="
+#
+# (sxs) d:\sxs>py src\head.py 150 dat\out\sample-cy.xml
+# <?xml version="1.0" encoding="UTF-8"?>
+# <Extract xmlns="http://www.nbb.be/cba/2021-06/extract">
+# ____<Header>
+# ________<DeclarerId>
+# ____________<Declarer
+#
+#
 chunk = int(sys.argv[1])
 ifile = sys.argv[2]
 f = open(ifile, encoding='utf-8')
