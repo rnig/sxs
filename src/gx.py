321ea205f1cb4b03840a2eb37b93684f5c8d1736
Erwin Geeraerts, bestuurder rnig bv <8044437+rnig@users.noreply.github.com>
2023-08-04 17:57:48 +0200

    Reworked the PlaceHolder/Literal logic.

--- src/gx.py
+++ src/gx.py
@@ -15,7 +15,14 @@ mode_map = { 'aan' : 'Add'
 template_replacing_sequence = [ 'map', 'exn' ] 
 
 # Convention.3:
-state = {}
+file_number = 1
+state = { 'FILE_REFERENCE' : Template('$DECLARER_KBO-INC-UAT-$TODAY-$FILE_NUMBER.xml')
+          , 'FILE_NUMBER' : "{0:0>3}".format(file_number)
+          , 'MODUS' : 'af'
+          , 'TODAY' : '20230804'
+          , 'DECLARER_KBO' : '0403288089'
+         }
+
 
 
 def load_template_data(path, filename, holder_data={}):
@@ -36,15 +43,20 @@ def load_template(path, filename):
     return Template(''.join(hy_io.load(hy_io.get_path([ path, filename.strip() ]))))
 
 
-def generate_declaration(template_path, template_map, data, mode):
+def generate_declaration(template_path, template_map, data):
+    global state
     declaration = []
     for template in hy_io.load(hy_io.get_path([ template_path, template_map ])):
         holder_data = load_template_data(template_path, template.strip().split('.')[0]+'.map')
         if hy_io.path_exists(hy_io.get_path([ template_path, template.strip().split('.')[0]+'.exn' ])):
-            holder_data_exn = load_template_data(template_path, template.split('.')[0]+'.exn', holder_data)
-        holder_data = holder_data_exn
+            holder_data = load_template_data(template_path, template.split('.')[0]+'.exn', holder_data)
         if 'Literal' == holder_data['PlaceHolder']:
             template_content = load_template(template_path, template)
+            for k in holder_data.keys():
+                print(k, holder_data[k])
+                if 'STATE' in holder_data[k]:
+                    print(state[holder_data[k].split(':')[1]])
+                    holder_data[k] = state[holder_data[k].split(':')[1]]
             declaration.append(template_content.safe_substitute(holder_data))
         elif 'Data' ==  holder_data['PlaceHolder']:
             data_header = data[0].strip().split(';')
@@ -56,10 +68,8 @@ def generate_declaration(template_path, template_map, data, mode):
                         phi = data_header.index(holder_data_row_copy[ph])
                         phv = row.split(';')[phi]
                         holder_data_row_copy[ph] = phv
-                #print(holder_data_row_copy)
-                holder_data_row_copy['Action'] = mode_map[mode]
-                holder_data_row_copy['Modus'] = mode_map[mode]+'Action'
-                #hy_is.confirm_to_continue()
+                holder_data_row_copy['Action'] = mode_map[state['MODUS']] # This is ugly but it works...
+                holder_data_row_copy['Modus'] = mode_map[state['MODUS']]+'Action' # This is very ugly but it works...
                 declaration.append(template_content.safe_substitute(holder_data_row_copy))
     return ''.join(declaration)
     
@@ -68,12 +78,12 @@ def generate_declaration(template_path, template_map, data, mode):
 if __name__ == '__main__':
     output = []
     modus = None
-    while modus not in [ 'aan', 'af' ]:
+    while modus not in mode_map.keys():
         modus = hy_is.ask_input("Wil je aanmelden [typ: 'aan'] of afmelden [typ: 'af']?").lower()
+    state['MODUS'] = modus
     output = generate_declaration(hy_is.query_path('naar de template folder')
                                   , hy_is.ask_input('Welke template map wil je gebruiken?')
-                                  , hy_io.load(hy_is.query_path('naar het bestand met de gegevens'))
-                                  , modus)
+                                  , hy_io.load(hy_is.query_path('naar het bestand met de gegevens')))
     output_folder = hy_is.query_path('naar de weergave folder')
     output_file = hy_is.ask_input('Hoe wil je het bestand benoemen?')
     if output:                              
